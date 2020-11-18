fa2dc1c9dc67d3ed749cad1e8069f51643d22a87
lebohang22 <mohlammelebohang22@gmail.com>
2020-11-18 12:10:59 +0200

    Another commit

---
+++ DimensionWebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DimensionWebApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DimensionWebApp.Areas.Identity.IdentityHostingStartup))]
+namespace DimensionWebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DimensionWebAppContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DimensionWebAppContextConnection")));
+
+                //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    //.AddEntityFrameworkStores<DimensionWebAppContext>();//
+            });
+        }
+    }
+}
